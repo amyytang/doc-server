a1c5d706ec9921b40beb9d57f2ad4980ffc36a25
Amy Tang <amyytang03@gmail.com>
2024-12-17 00:27:00 -0800

    update ignored files

--- jobapi/views.py
+++ jobapi/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from processor.query import query
 
 def index(request):
     query_param_value = request.GET.get('job')
-    return HttpResponse(f"The value of param_name is: {query_param_value}")
+    if query_param_value is None:
+        return HttpResponse("Request parameter is missing!", status=400)
+    
+    #result = job(query_param_value)
+    result = query(query_param_value)
+    return HttpResponse(result)
