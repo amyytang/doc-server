f5e98b2f356c58f1edccca9c24fe0d7bc15edc41
Amy Tang <amyytang03@gmail.com>
2024-12-17 00:02:53 -0800

    Removed extra files

--- queryapi/jobapi/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    query_param_value = request.GET.get('job')
-    return HttpResponse(f"The value of param_name is: {query_param_value}")
